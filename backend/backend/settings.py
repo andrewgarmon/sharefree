860052c125464eac0aef4e5f51cc9a3d2681798d
Andrew Garmon <andrew.garmon@gmail.com>
2020-12-13 14:26:47 -0500

    added secret key to .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
 """
 Django settings for backend project.
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@81d#y3t5jarw)rjnuswy1i9ijy+3futjlul*s2(tj@hchofgg'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
