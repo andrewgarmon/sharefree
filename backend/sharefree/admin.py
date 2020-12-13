edf512577f79cd2b0bb97c4d6b2c01429a390269
Andrew Garmon <andrew.garmon@gmail.com>
2020-12-13 17:39:10 -0500

    Added models for users, stocks, portfolios

--- backend/sharefree/admin.py
+++ backend/sharefree/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Portfolio, Stock, User, Share
 # Register your models here.
+
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('user', 'value')
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'price')
+
+class ShareAdmin(admin.ModelAdmin):
+    list_display = ('portfolio', 'stock', 'shares')
+
+admin.site.register(Portfolio, PortfolioAdmin)
+admin.site.register(Stock, StockAdmin)
+admin.site.register(Share, ShareAdmin)
--- backend/sharefree/models.py
+++ backend/sharefree/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Stock(models.Model):
+    symbol = models.CharField(max_length=5, primary_key=True)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+
+class Portfolio(models.Model):
+    value = models.DecimalField(decimal_places=2, max_digits=15)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+class Share(models.Model):
+    portfolio = models.ForeignKey(
+        Portfolio,
+        on_delete=models.CASCADE,
+    )
+    stock = models.ForeignKey(
+        Stock,
+        on_delete=models.CASCADE,
+    )
+    shares = models.PositiveIntegerField
+
+class User(models.Model):
+    user = User.objects.create_user
